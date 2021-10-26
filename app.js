f8f6d9b04ca6ce577bc0746835f91569d71936a5
Adrian <confuzedfx@gmail.com>
2021-10-26 15:00:39 -0700

    add orig and compressed images

--- app.js
+++ app.js
@@ -16,23 +16,16 @@ app.set('view engine', 'jade');
 
 const shouldCompress = (req, res) => {
   if (req.headers['x-no-compression']) {
-    // Will not compress responses, if this header is present
     return false;
   }
-  // Resort to standard compression
   return compression.filter(req, res);
 };
 // Compress all HTTP responses
 app.use(compression({
-  // filter: Decide if the answer should be compressed or not,
-  // depending on the 'shouldCompress' function above
   filter: shouldCompress,
-  // threshold: It is the byte threshold for the response
-  // body size before considering compression, the default is 1 kB
   threshold: 0
 }));
 
-app.use(compression())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
