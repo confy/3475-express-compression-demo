44f1e34e9862586728e0fd696ce32a29fac7da8b
Adrian <confuzedfx@gmail.com>
2021-10-26 14:17:45 -0700

    Mostly finished demo

---
+++ app.js
@@ -0,0 +1,61 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const compression = require('compression');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+const shouldCompress = (req, res) => {
+  if (req.headers['x-no-compression']) {
+    // Will not compress responses, if this header is present
+    return false;
+  }
+  // Resort to standard compression
+  return compression.filter(req, res);
+};
+// Compress all HTTP responses
+app.use(compression({
+  // filter: Decide if the answer should be compressed or not,
+  // depending on the 'shouldCompress' function above
+  filter: shouldCompress,
+  // threshold: It is the byte threshold for the response
+  // body size before considering compression, the default is 1 kB
+  threshold: 0
+}));
+
+app.use(compression())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
